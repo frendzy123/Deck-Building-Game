857f47c4faff0265d21408208b575c1b9e781759
Xavier Chua <xavierc.1998@gmail.com>
2018-06-15 14:41:42 -0700

    Made changes to ui in another scene
    Made a new scene with enemy as only image. Changed animation from sprites to images. Made hand and draggable cards that destroy on drop on enemies.

---
+++ Assets/Resources/Scripts/Draggable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	Transform returnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData) 
+	{
+
+		returnTo = this.transform.parent;
+		this.transform.SetParent(this.transform.parent.parent); //Takes it outside of hand
+		//Debug.Log ("OnBeginDrag");
+
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		//Debug.Log ("Drag");
+
+		this.transform.position = eventData.position; 
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		//Debug.Log ("End drag");
+		this.transform.SetParent(returnTo);
+
+		List<RaycastResult> hits = new List<RaycastResult>();
+
+		EventSystem.current.RaycastAll(eventData, hits);
+
+		for (int i = 0; i < hits.Count; i++) {
+			if (hits [i].gameObject.tag == "Enemy") {
+				Destroy (this.gameObject);
+			}
+		}
+
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+
+}
---
+++ Assets/Resources/Scripts/DropZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		//Debug.Log ("pointer enter");
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		//Debug.Log ("pointer exit");
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log ("dropped to " + gameObject.name);
+	}
+}
